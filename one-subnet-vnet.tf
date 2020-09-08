31e4c6bd9b077b12ec3c44a145a6c945f5f52944
santhosha021 <39211711+santhosha021@users.noreply.github.com>
2020-09-08 15:35:42 +1000

    Update one-subnet-vnet.tf

--- one-subnet-vnet.tf
+++ one-subnet-vnet.tf
@@ -21,3 +21,23 @@ resource "azurerm_subnet" "vnetsubnet" {
     virtual_network_name =azurerm_virtual_network.test.name
     address_prefixes = ["10.100.100.0/25"]
 }
+resource "azurerm_network_interface" "nic01" {
+  name                = "demo-nic01"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.vnetsubnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+resource "azurerm_network_security_group" "nsg01" {
+  name                = "workload-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.nic01.id
+  network_security_group_id = azurerm_network_security_group.nsg01.id
+}
