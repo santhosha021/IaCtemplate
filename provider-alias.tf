03965d99f6e367cb2d2c64ad93ebf73acf0f986e
santhosha021 <39211711+santhosha021@users.noreply.github.com>
2020-09-10 12:22:06 +1000

    Create provider-alias.tf

---
+++ provider-alias.tf
@@ -0,0 +1,13 @@
+provider "azurerm" {
+  features {}
+  alias           = "santhosh"
+  version         = ">2.21.0"
+  subscription_id = "sssssssssssssss"
+}
+
+provider "azurerm" {
+  features {}
+  alias           = "vsprofile"
+  version         = ">2.21.0"
+  subscription_id = "yyyyyyyyyyyy"
+}
